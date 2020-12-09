8582222cc38338590bbd88eaa5478c3914a499d1
almog ram <almoghr36@gmail.com>
2020-12-09 11:35:38 +0200

    added sockets, listeners, emitters and everything to start working on the mesages, later on - the messages are rendered with a timestamp and now done styling the chat app

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,68 @@
-io()
+const socket = io() // הקליינט מדבר אל השרת
+
+// Elements
+const messageForm = document.querySelector('#message-form')
+const messageFormInput = document.querySelector('#message-form-input')
+const messageFormButton = document.querySelector('#message-form-button')
+const locationButton = document.querySelector('#location-button')
+const messages = document.querySelector('#messages')
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const messageLocationTemplate = document.querySelector('#message-location-template').innerHTML
+
+
+socket.on('message', (message) => {
+    console.log(message)
+
+    const html = Mustache.render(messageTemplate, {
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a'),
+    })
+    messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (location) => {
+    console.log(location)
+
+    const html = Mustache.render(messageLocationTemplate, {
+        url: location.url,
+        createdAt: moment(location.createdAt).format('h:mm a')
+    })
+    messages.insertAdjacentHTML('beforeend', html)
+})
+
+
+
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    messageFormButton.setAttribute('disabled', 'disabled')
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, (error) => {
+    messageFormButton.removeAttribute('disabled')
+    messageFormInput.value = ''
+    messageFormInput.focus()
+        if(error){
+            return console.log(error)
+        }
+        console.log('message sent')
+        
+    })
+})
+
+locationButton.addEventListener('click', (e) => {
+    if(!navigator.geolocation){
+        return alert('Geolocation is not supported by your browser')
+    } 
+    locationButton.setAttribute('disabled', 'disabled')
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, (msg) => {
+            locationButton.removeAttribute('disabled')
+            console.log(msg)
+        })
+    })
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const path = require('path')
 const http = require ('http')
 const express = require ('express')
 const socketio = require ('socket.io')
+const Filter = require ('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -10,12 +12,36 @@ const io = socketio(server)
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
-// app.use(express.json())
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', (socket) => {
+io.on('connection', (socket) => { // השרת מדבר אל הקליינט
     console.log('New WebSocket connection established.')
+
+    const welcomeMessage = 'welcome dear user to my chat app'
+    socket.emit('message', generateMessage(welcomeMessage)) // רק המשתמש הספציפי שמחובר מהקליינט עצמו יראה את ההודעה
+    socket.broadcast.emit('message', generateMessage('A new user has joined')) //שולח לכולם הודעה פרט למשתמש שהתחבר כעת.
+
+    socket.on('sendMessage', (message, cb) => {
+        const filter = new Filter()
+        if(filter.isProfane(message)){
+            return cb('Foul language is not allowed.')
+        }
+        io.emit('message', generateMessage(message)) // כל המשתמשים יראו את ההודעה
+        cb()
+    })
+
+    socket.on('sendLocation', (coords, cb) => {
+        const url = `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+        io.emit('locationMessage', generateLocationMessage(url)) 
+        cb('location shared')
+    })
+    
+    socket.on('disconnect', () => {
+        io.emit('message', generateMessage('a user has left'))
+    })
 })
+
+
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
